29457691afaf607cd95975db3896bc2bcbd641fa
gaobingcug <141312395+gaobingcug@users.noreply.github.com>
2024-07-12 19:24:03 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+# This code file is the main structure of the network and can be viewed against Fig.1 in the paper
+
 import kornia
 from mmcv.ops.modulated_deform_conv import ModulatedDeformConv2dPack as DCN
 from othermodels import *
